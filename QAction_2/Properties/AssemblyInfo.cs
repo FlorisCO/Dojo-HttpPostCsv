535f06c9a836f3436730ee4596f621e91da83cb1
Floris Cockaerts <floris.cockaerts@skyline.be>
2023-08-16 10:58:38 +0200

    Add project files.

---
+++ QAction_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QAction_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("QAction_2")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F42A6CDE-A066-4705-8B3B-C85E075B9833")]
+[assembly: AssemblyVersion("1.0.0.0")]
